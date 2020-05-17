ee5d169b84344dbde7159109c969f179d72b7a81
Kalyvas Emmanouil <33424179+MnAppsNet@users.noreply.github.com>
2020-05-17 15:03:52 +0300

    Initial Commit

--- KeyboardEvents.cs
+++ KeyboardEvents.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -6,9 +6,7 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Windows.Forms;
 
-namespace MultiBin
-{
-    class eventHandler
+class eventHandler
     {
         public bool Triggered { get { return triggered; } set { triggered = value; } }
         public string KeyStroke { get { return keyStroke; } }
@@ -38,116 +36,115 @@ namespace MultiBin
         }
     }
 
-    class KeyboardEvents
+class KeyboardEvents
+{
+    private const bool LOG_KEYS = true; //< Logs keys in console if set to true
+
+    private const int WH_KEYBOARD_LL = 13;
+    private const int WM_KEYDOWN = 0x0100;
+    private const int WM_KEYUP = 0x0101;
+    private static List<Keys> keysDown;
+    private static LowLevelKeyboardProc _proc = HookCallback;
+    private static IntPtr _hookID = IntPtr.Zero;
+    private static List<eventHandler> events;
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+    [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+    public KeyboardEvents()
     {
-        private const bool LOG_KEYS = true; //< Logs keys in console if set to true
+        keysDown = new List<Keys>();
+        events = new List<eventHandler>();
+    }
 
-        private const int WH_KEYBOARD_LL = 13;
-        private const int WM_KEYDOWN = 0x0100;
-        private const int WM_KEYUP = 0x0101;
-        private static List<Keys> keysDown;
-        private static LowLevelKeyboardProc _proc = HookCallback;
-        private static IntPtr _hookID = IntPtr.Zero;
-        private static List<eventHandler> events;
+    public void AddEvent(string name, Keys[] keyStroke, object instance, string method, object[] parameters)
+    {
+        eventHandler evnt = new eventHandler(name, getKeyStrokeString(keyStroke), instance, method, parameters);
+        events.Add(evnt);
+    }
 
-        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-        private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+    public void CaptureKeyboard()
+    {
+        _hookID = SetHook(_proc);
+    }
+    public void StopCapturingKeyboard()
+    {
+        UnhookWindowsHookEx(_hookID);
+    }
 
-        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
-
-        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
-
-        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-        private static extern IntPtr GetModuleHandle(string lpModuleName);
-
-        public KeyboardEvents()
+    private static IntPtr SetHook(LowLevelKeyboardProc proc)
+    {
+        using (Process curProcess = Process.GetCurrentProcess())
+        using (ProcessModule curModule = curProcess.MainModule)
         {
-            keysDown = new List<Keys>();
-            events = new List<eventHandler>();
+            return SetWindowsHookEx(WH_KEYBOARD_LL, proc,
+                GetModuleHandle(curModule.ModuleName), 0);
         }
-
-        public void AddEvent(string name, Keys[] keyStroke, object instance, string method, object[] parameters)
+    }
+    private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+    private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+    {
+        if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
         {
-            eventHandler evnt = new eventHandler( name, getKeyStrokeString(keyStroke), instance, method, parameters);
-            events.Add(evnt);
-        }
-
-        public void CaptureKeyboard()
-        {
-            _hookID = SetHook(_proc);
-        }
-        public void StopCapturingKeyboard()
-        {
-            UnhookWindowsHookEx(_hookID);
-        }
-
-        private static IntPtr SetHook(LowLevelKeyboardProc proc)
-        {
-            using (Process curProcess = Process.GetCurrentProcess())
-            using (ProcessModule curModule = curProcess.MainModule)
+            int vkCode = Marshal.ReadInt32(lParam);
+            Keys vk = (Keys)vkCode;
+            if (!keysDown.Contains(vk))
             {
-                return SetWindowsHookEx(WH_KEYBOARD_LL, proc,
-                    GetModuleHandle(curModule.ModuleName), 0);
+                keysDown.Add((Keys)vkCode);
+                string keyStrokeString = getKeyStrokeString(keysDown.ToArray());
+                eventHandler e = events.Find(evnt => (evnt.Triggered == false && evnt.KeyStroke == keyStrokeString));
+                if (e != null) e.call();
+
+                if (LOG_KEYS)
+                    Console.WriteLine("KeyDown : " + vk);
             }
         }
-        private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
-        private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        else if (nCode >= 0 && wParam == (IntPtr)WM_KEYUP)
         {
-            if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+            int vkCode = Marshal.ReadInt32(lParam);
+            Keys vk = (Keys)vkCode;
+            if (keysDown.Contains(vk))
             {
-                int vkCode = Marshal.ReadInt32(lParam);
-                Keys vk = (Keys)vkCode;
-                if (!keysDown.Contains(vk))
+                string keyStrokeString = getKeyStrokeString(keysDown.ToArray());
+                if (keyStrokeString == "")
                 {
-                    keysDown.Add((Keys)vkCode);
-                    string keyStrokeString = getKeyStrokeString(keysDown.ToArray());
-                    eventHandler e = events.Find(evnt => ( evnt.Triggered == false && evnt.KeyStroke == keyStrokeString ));
-                    if (e != null) e.call();
-
-                    if(LOG_KEYS)
-                     Console.WriteLine("KeyDown : " + vk);
+                    foreach (eventHandler ev in events.FindAll(evnt => (evnt.Triggered == true)))
+                    {
+                        ev.Triggered = false;
+                    }
                 }
-            }
-            else if (nCode >= 0 && wParam == (IntPtr)WM_KEYUP)
-            {
-                int vkCode = Marshal.ReadInt32(lParam);
-                Keys vk = (Keys)vkCode;
-                if (keysDown.Contains(vk))
+                else
                 {
-                    string keyStrokeString = getKeyStrokeString(keysDown.ToArray());
-                    if(keyStrokeString == "")
-                    {
-                        foreach( eventHandler ev in events.FindAll(evnt => (evnt.Triggered == true)))
-                        {
-                            ev.Triggered = false;
-                        }
-                    }
-                    else
-                    {
-                      eventHandler e = events.Find(evnt => (evnt.Triggered == true && evnt.KeyStroke == keyStrokeString));
-                      if (e != null) e.Triggered = false;
-                    }
-                    
-                    keysDown.Remove((Keys)vk);
-                    
-                    if (LOG_KEYS)
-                     Console.WriteLine("KeyUp : " + (Keys)vkCode);
+                    eventHandler e = events.Find(evnt => (evnt.Triggered == true && evnt.KeyStroke == keyStrokeString));
+                    if (e != null) e.Triggered = false;
                 }
-            }
 
-            return CallNextHookEx(_hookID, nCode, wParam, lParam);
-        }
-        private static string getKeyStrokeString(Keys[] keys)
-        {
-            string keyStrokeString = "";
-            foreach (Keys k in keys)
-            {
-                keyStrokeString = keyStrokeString + ((int)k).ToString();
+                keysDown.Remove((Keys)vk);
+
+                if (LOG_KEYS)
+                    Console.WriteLine("KeyUp : " + (Keys)vkCode);
             }
-            return keyStrokeString;
         }
+
+        return CallNextHookEx(_hookID, nCode, wParam, lParam);
+    }
+    private static string getKeyStrokeString(Keys[] keys)
+    {
+        string keyStrokeString = "";
+        foreach (Keys k in keys)
+        {
+            keyStrokeString = keyStrokeString + ((int)k).ToString();
+        }
+        return keyStrokeString;
     }
 }
